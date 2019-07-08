0f263888ef7fb8acb7a958f47f91bab5dbcdcaed
David <damorales310@gmail.com>
2019-07-08 02:32:54 -0600

    Created roll_curator func.
    This function will iterate through each line of the first column of the csv and add the first four letters of each line to a list if those letters (mags), don't already exist within the list.
    
    Finally returning the list.

---
+++ src/extract.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import csv
+import os
+import re
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+csv_path = os.path.join(dir_path, 'CSV', 'test.csv')
+
+data = pd.read_csv(csv_path, usecols=[0])
+test = data.values.tolist()
+
+
+def roll_curator():
+
+    roll_list = ['START']
+
+    for line in test:
+        for item in line:
+            if item[:4] == roll_list[-1]:
+                continue
+            else:
+                roll_list.append(item[:4])
+
+    return roll_list
+
+rolls = roll_curator()
+        
+
