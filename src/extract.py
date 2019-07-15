cad1fdea4931b43f01651f65aef1fbe7ac3f6bf2
David Morales <damorales310@gmail.com>
2019-07-14 23:26:23 -0600

    Polishing
    cr_extractor
    Added docstring.
    
    sr_cruator
    Added docstring. Added comments.
    
    All functions get called at the end of the script.

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import csv
 import os
 import re
 
@@ -17,8 +16,7 @@ def roll_curator():
         [list] -- [contains first four digits of the first column]
     """
 
-    roll_list = ['START']
-
+    roll_list = ['']
 
     for line in test:
         for item in line:
@@ -29,14 +27,14 @@ def roll_curator():
 
     return roll_list
 
-rolls = roll_curator()
-
 
 def cr_extractor(lst):
-    """[summary]
+    """[The roll list gets convertered to a string, and then a regex is used
+        to search through the string to find a match object. The results are
+        printed to a text file called rolls.text]
 
     Arguments:
-        lst {list} -- [description]
+        lst {list} -- [a list containing potential camera rolls]
     """
 
     roll_str = ','.join(lst)
@@ -45,45 +43,52 @@ def cr_extractor(lst):
     mo = camregex.findall(roll_str)
     mo.sort()
 
-    f = open("rolls.text", "w")
-
+    f = open(dir_path + "rolls.text", "w")
     f.write("Camera Rolls: \n")
     for roll in mo:
         f.write(roll + "\n")
     f.close()
 
-cr_extractor(rolls)
 
 def sr_curator():
+    """[Searches through column five, for the sound_masters path and appends
+        the last three digits of the path to rolls.text.]
+    """
 
     col_5 = pd.read_csv(csv_path, usecols=[5])
     col_5_lst = col_5.values.tolist()
 
+    #* Iterates through col_5_lst twice to grab the strings.
     path_lst = []
     for line in col_5_lst:
         for item in line:
             path_lst.append(item)
 
+    #* Iterates through every row till it finds sound_masters
+    #* Stores index number in sm_arry for later use
     smregex = re.compile(r'sound_masters', re.IGNORECASE)
-    rcounter = 0
+    i = 0
     sm_arry = []
     for row in path_lst:
         if smregex.search(row):
-            sm_arry.append(rcounter)
-        rcounter += 1
+            sm_arry.append(i)
+        i += 1
 
-
+    #* Adds the last three chars of every row that was indexed
+    #* inside of sm_array. Stores Sound Rolls in sr_rolls[]
     i = 0
     sr_rolls = []
     while i < len(sm_arry):
         sr_rolls.append(path_lst[sm_arry[i]][-3:])
         i += 1
 
-    f = open('rolls.text', 'a+')
+    f = open(dir_path + 'rolls.text', 'a+')
     f.write('\nSound Rolls: \n')
     for roll in sr_rolls:
         f.write(roll + '\n')
     f.close()
 
 
+rolls = roll_curator()
+cr_extractor(rolls)
 sr_curator()
